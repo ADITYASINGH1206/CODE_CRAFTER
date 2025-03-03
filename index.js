8dc2a760ee348596660c035807b26ebc4cf7c1a3
ADITYASINGH1206 <simghaditya21@gmail.com>
2025-03-03 11:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("myb")
 const user = document.getElementById("user")
 const pass = document.getElementById("pass")
-const val = document.getElementById("val")
+const val =  document.getElementById("val")
 
 button.onclick = function(){
     if(user.value == "admin" && pass.value == "admin"){
